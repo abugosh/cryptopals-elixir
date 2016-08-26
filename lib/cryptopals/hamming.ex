148ec7be3a38186f582a72f6919b293124cf1091
Alex Bugosh <abugosh@gmail.com>
2016-08-26 16:28:02 -0500

    Adding hamming code

---
+++ lib/cryptopals/hamming.ex
@@ -0,0 +1,24 @@
+
+defmodule Cryptopals.Hamming do
+  use Bitwise
+
+  def distance(bin1, bin2) when bin1 == bin2 do
+    0
+  end
+
+  def distance(bin1, bin2) do
+    Cryptopals.XorHelpers.xor(bin1, bin2)
+    |> Enum.map(&sum_byte_bits/1)
+    |> Enum.sum
+  end
+
+  defp sum_byte_bits(byte) do
+    [1, 2, 4, 8, 16, 32, 64, 128]
+    |> Enum.map(&(bits_up?(byte, &1)))
+    |> Enum.count(&(&1))
+  end
+
+  defp bits_up?(byte, val) do
+    (byte &&& val) > 0
+  end
+end
---
+++ spec/cryptopals/hamming_spec.exs
@@ -0,0 +1,22 @@
+
+defmodule Cryptopals.HammingSpec do
+  use ESpec
+  let :bin1, do: 'this is a test'
+  let :bin2, do: 'wokka wokka!!!'
+
+  subject(Cryptopals.Hamming)
+
+  context ".distance" do
+    it "should have a distance of 0 if the strings are equal" do
+      expect(subject.distance('AAAA', 'AAAA')) |> to(eq 0)
+    end
+
+    it "should have a distance of byte size if the strings are maximally different" do
+      expect(subject.distance([0, 0, 0, 0], [0xff, 0xff, 0xff, 0xff])) |> to(eq 32)
+    end
+
+    it "should have a distance of 37 for our test strings" do
+      expect(subject.distance(bin1, bin2)) |> to(eq 37)
+    end
+  end
+end
