00571e4bcc64a03b7a2e09b43f87b2162eaac52b
Alex Bugosh <abugosh@gmail.com>
2016-08-29 10:52:35 -0500

    Solving challenge 9

---
+++ spec/acceptance/challenge_9_spec.exs
@@ -0,0 +1,12 @@
+
+defmodule Cryptopals.Challenge9Spec do
+  use ESpec
+  alias Cryptopals.Pkcs7Helpers
+
+  let :txt_to_pad, do: 'YELLOW SUBMARINE'
+  let :padded_txt, do: 'YELLOW SUBMARINE' ++ [4, 4, 4, 4]
+
+  it "pads the txt to 20 bytes" do
+    expect(Pkcs7Helpers.pad_block(txt_to_pad, 20)) |> to(eq padded_txt)
+  end
+end
