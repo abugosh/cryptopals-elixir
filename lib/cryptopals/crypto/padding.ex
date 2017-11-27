26c9217bfa3d1e9687464d2b978fa943af8fdfb9
Alex Bugosh <abugosh@gmail.com>
2017-11-26 18:24:29 -0600

    Adding a function to do padding of a full text

--- lib/cryptopals/crypto/padding.ex
+++ lib/cryptopals/crypto/padding.ex
@@ -1,7 +1,17 @@
 defmodule Cryptopals.Crypto.Padding do
+  def pad_block([], _), do: []
+  def pad_block(block, len) when length(block) == len, do: block
   def pad_block(block, len) do
     padding_length = len - Enum.count(block)
 
     block ++ ([padding_length] |> Stream.cycle |> Enum.take(padding_length))
+  end
+
+  def pad_text(txt, block_size) do
+    base = (txt |> Enum.count |> div(block_size)) * block_size
+
+    {head, tail} = txt |> Enum.split(base)
+
+    head ++ pad_block(tail, block_size)
   end
 end
--- spec/cryptopals/crypto/padding_spec.exs
+++ spec/cryptopals/crypto/padding_spec.exs
@@ -4,15 +4,51 @@ defmodule Cryptopals.Crypto.PaddingSpec do
 
   context ".pad_block" do
     it "does nothing if the txt matches the block size" do
-      expect(described_module().pad_block('hello', 5)) |> to(eq 'hello')
+      'hello'
+      |> described_module().pad_block(5)
+      |> expect
+      |> to(eq 'hello')
     end
 
     it "pads the text with bytes of the length of the padding" do
-      expect(described_module().pad_block('hello', 6)) |> to(eq 'hello' ++ [0x01])
+      'hello'
+      |> described_module().pad_block(6)
+      |> expect
+      |> to(eq 'hello' ++ [0x01])
     end
 
     it "adds bytes up to the block size if the txt is smaller" do
-      expect(described_module().pad_block('hello', 10)) |> to(eq 'hello' ++ [0x05, 0x05, 0x05, 0x05, 0x05])
+      'hello'
+      |> described_module().pad_block(10)
+      |> expect
+      |> to(eq 'hello' ++ [0x05, 0x05, 0x05, 0x05, 0x05])
+    end
+  end
+
+  context ".pad_text" do
+    it "does nothing if the text matches the block size" do
+      'hello'
+      |> described_module().pad_text(16)
+      |> expect
+      |> to(eq described_module().pad_block('hello', 16))
+    end
+
+    it "does nothing if the text's size is a multiple of the block size" do
+      txt = 'hello' |> Stream.cycle |> Enum.take(10)
+
+      txt
+      |> described_module().pad_text(5)
+      |> expect
+      |> to(eq txt)
+    end
+
+    it "pads the final block to the size of the block size if it is less than the block size" do
+      txt = 'hello' |> Stream.cycle |> Enum.take(14)
+
+      txt
+      |> described_module().pad_text(5)
+      |> expect
+      |> to(eq txt ++ [0x01])
     end
   end
 end
